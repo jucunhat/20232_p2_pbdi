f64b819621501f47c2920424ced406da231077f3
Julia Cunha <juliacunhatassini@gmail.com>
2023-11-14 10:27:51 -0300

    feat(tg): dados valiosos

--- script.sql
+++ script.sql
@@ -1,10 +1,37 @@
--- Exercicio 4
-CREATE TABLE log(
-  cod_log SERIAL PRIMARY KEY,
-  nome_youtuber varchar(200),
-  categoria VARCHAR(200),
-  inicio INT  
-);
+-- Exercicio 5
+CREATE OR REPLACE TRIGGER previne_delete
+BEFORE DELETE ON tb_top_youtubers
+FOR EACH ROW
+EXECUTE PROCEDURE fn_previne_delete();
+
+CREATE OR REPLACE FUNCTION fn_previne_delete() RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+     INSERT INTO log(nome_youtuber, categoria, inicio)
+     SELECT youtuber, category, started
+     FROM tb_top_youtubers
+     WHERE cod_top_youtubers = OLD.cod_top_youtubers;
+
+     UPDATE tb_top_youtubers
+     SET ativo = 0
+     WHERE cod_top_youtubers = OLD.cod_top_youtubers;
+
+     RETURN NULL;
+END;
+$$;
+
+-- -- Testando a exclusão
+-- DELETE FROM tb_top_youtubers WHERE cod_top_youtubers = 100;
+-- SELECT * FROM log
+
+-- -- Exercicio 4
+-- CREATE TABLE log(
+--   cod_log SERIAL PRIMARY KEY,
+--   nome_youtuber varchar(200),
+--   categoria VARCHAR(200),
+--   inicio INT  
+-- );
 
 -- -- Exercicio 3
 -- ALTER TABLE tb_top_youtubers ADD COLUMN ativo INTEGER DEFAULT 1 CHECK (ativo IN (0, 1));
